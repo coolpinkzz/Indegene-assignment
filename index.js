011c5b33020c925f4f099424c7d874afea2c260a
ASUS <ASUS@LAPTOP-UNDN5F42>
2021-01-20 00:51:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const btn1 = document.querySelector('input[id="1"]');
+const btn2 = document.querySelector('input[id="2"]');
+const btn3 = document.querySelector('input[id="3"]');
+const carousel = document.querySelector(".carousel");
+
+btn1.addEventListener("click", () => {
+    carousel.style.transform = "translateX(0)";
+});
+
+btn2.addEventListener("click", () => {
+    carousel.style.transform = "translateX(-100%)";
+});
+
+btn3.addEventListener("click", () => {
+    carousel.style.transform = "translateX(-200%)";
+});
+
+
+var nav = document.getElementById('nav')
+
+function togglemenu() {
+    console.log("click")
+    if (nav.style.display === "grid") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "grid";
+    }
+
+
+}
+
+
